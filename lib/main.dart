2b3f4e9a6424a10d146495cd77caec7c85f3b56c
heyue <vipheyue@foxmail.com>
2025-06-14 11:48:30 +0800

    添加界面

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,248 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+
+// 按钮状态类
+class ButtonState {
+  final String text;
+  final String? backgroundImage;
+  final Color backgroundColor;
+  final ButtonType buttonType;
+  final int number;
+
+  ButtonState({
+    required this.text,
+    this.backgroundImage,
+    required this.backgroundColor,
+    required this.buttonType,
+    required this.number,
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// 按钮类型枚举
+enum ButtonType {
+  normal,
+  highlighted,
+  disabled,
+  special,
+}
 
-  // This widget is the root of your application.
+class GridButtonScreen extends StatefulWidget {
+  const GridButtonScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<GridButtonScreen> createState() =>  _GridButtonScreenState();
+
+}
+
+class _GridButtonScreenState extends State<GridButtonScreen> {
+  // 创建20个按钮的状态集合 (4x5 = 20个按钮)
+  late List<ButtonState> buttonStates;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeButtonStates();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initializeButtonStates() {
+    buttonStates = List.generate(20, (index) {
+      // 根据索引生成不同的状态
+      return ButtonState(
+        text: '按钮${index + 1}',
+        backgroundColor: _getColorByIndex(index),
+        buttonType: _getButtonTypeByIndex(index),
+        number: index + 1,
+      );
     });
   }
 
+  Color _getColorByIndex(int index) {
+    List<Color> colors = [
+      Colors.blue,
+      Colors.green,
+      Colors.orange,
+      Colors.red,
+      Colors.purple,
+      Colors.teal,
+      Colors.pink,
+      Colors.indigo,
+    ];
+    return colors[index % colors.length];
+  }
+
+  ButtonType _getButtonTypeByIndex(int index) {
+    if (index % 4 == 0) return ButtonType.special;
+    if (index % 3 == 0) return ButtonType.highlighted;
+    if (index % 7 == 0) return ButtonType.disabled;
+    return ButtonType.normal;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('网格按钮布局'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // 横向4个按钮
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+            childAspectRatio: 1.0, // 正方形按钮
+          ),
+          itemCount: 20, // 总共20个按钮 (4x5)
+          itemBuilder: (context, index) {
+            return _buildGridButton(buttonStates[index], index);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildGridButton(ButtonState state, int index) {
+    return Container(
+      decoration: BoxDecoration(
+        color: state.backgroundColor,
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(
+          color: _getBorderColor(state.buttonType),
+          width: 2.0,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8.0),
+          onTap: state.buttonType != ButtonType.disabled
+              ? () => _onButtonPressed(state, index)
+              : null,
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 显示数字
+                Text(
+                  '${state.number}',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: _getTextColor(state.buttonType),
+                  ),
+                ),
+                SizedBox(height: 4),
+                // 显示文本
+                Text(
+                  state.text,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: _getTextColor(state.buttonType),
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                // 显示按钮类型图标
+                SizedBox(height: 4),
+                Icon(
+                  _getIconByButtonType(state.buttonType),
+                  size: 16,
+                  color: _getTextColor(state.buttonType),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color _getBorderColor(ButtonType type) {
+    switch (type) {
+      case ButtonType.highlighted:
+        return Colors.amber;
+      case ButtonType.disabled:
+        return Colors.grey;
+      case ButtonType.special:
+        return Colors.white;
+      default:
+        return Colors.transparent;
+    }
+  }
+
+  Color _getTextColor(ButtonType type) {
+    switch (type) {
+      case ButtonType.disabled:
+        return Colors.grey[600]!;
+      default:
+        return Colors.white;
+    }
+  }
+
+  IconData _getIconByButtonType(ButtonType type) {
+    switch (type) {
+      case ButtonType.highlighted:
+        return Icons.star;
+      case ButtonType.disabled:
+        return Icons.block;
+      case ButtonType.special:
+        return Icons.diamond;
+      default:
+        return Icons.touch_app;
+    }
+  }
+
+  void _onButtonPressed(ButtonState state, int index) {
+    setState(() {
+      // 点击时改变按钮状态，例如切换颜色
+      buttonStates[index] = ButtonState(
+        text: state.text,
+        backgroundColor: state.backgroundColor == Colors.blue
+            ? Colors.green
+            : Colors.blue,
+        buttonType: state.buttonType,
+        number: state.number,
+      );
+    });
+
+    // 显示点击反馈
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('点击了${state.text} - 数字: ${state.number}'),
+        duration: Duration(milliseconds: 800),
+      ),
+    );
+  }
+}
+
+// 主应用入口
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 网格按钮',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GridButtonScreen(),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
 }
